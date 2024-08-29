cd1ad83fb09a3d0cbb54f02ca55bc77c1fac7599
trang <trangbg2000@gmail.com>
2024-08-29 15:30:25 +0700

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { cn } from './utils/cn';
+import { isIOS, isWeb } from './utils/platform';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className='flex items-center h-screen'>
+      <Text className={cn(isWeb && 'text-green-300', isIOS && 'text-red-500')}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
